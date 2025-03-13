ae35f506ec1b9beff35bed820e84acc784a51de9
Fracisco Acosta <rwilly203@gmail.com>
2025-03-13 13:13:29 -0600

    Seccion 6 finishing
    end of seccion 6 with local storage, get data, inputs , outputs and create wach component

--- bases/src/app/components/dragonball/dragonball-character-add/dragonball-character-add.component.ts
+++ bases/src/app/components/dragonball/dragonball-character-add/dragonball-character-add.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import { Character } from '../../../interfaces/character.interface';
 
 @Component({
   selector: 'dragonball-character-add',
@@ -7,10 +8,32 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DragonballCharacterAddComponent {
-  name = input.required<string>();
-  power = input.required<number>();
+  name = signal('');
+  power = signal(0);
 
-  changes(){
+  newCharacter = output<Character>();
+
+  addCharacter() {
+    if( !this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      //id: this.characters().length + 1,
+      id: Math.floor(Math.random() * 1000),
+      name: this.name(),
+      power: this.power(),
+    };
+
+    //this.characters.update((list) => [...list, newCharacter]);
+    this.newCharacter.emit(newCharacter);
+    this.resetFields();
 
   }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
 }
--- bases/src/app/pages/dragonball-super/dragonball-super.component.ts
+++ bases/src/app/pages/dragonball-super/dragonball-super.component.ts
@@ -1,51 +1,28 @@
 
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/characterList/characterList.component";
 import { DragonballCharacterAddComponent } from "../../components/dragonball/dragonball-character-add/dragonball-character-add.component";
+import { DragonballService } from '../../services/dragonball.service';
+import { Character } from '../../interfaces/character.interface';
 
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+
 
 
 @Component({
   selector: 'app-dragonballSuper',
 
   templateUrl: './dragonball-super.component.html',
-  imports: [CharacterListComponent, DragonballCharacterAddComponent],
+  imports: [
+    CharacterListComponent,
+    DragonballCharacterAddComponent
+  ],
 })
 export class DragonballSuperComponent {
 
-  name = signal('');
-  power = signal<number>(0);
+  // constructor(
+  //   private dragonballService: DragonballService
+  // ){}
 
-
-  characters = signal<Character[]>([  // es una señal que manerajara un arreglo de personajes
-    { id: 1, name: 'Goku', power: 15001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-  addCharacter() {
-    if( !this.name() || !this.power() || this.power() <= 0) {
-      return;
-    }
-
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power(),
-    };
-
-    this.characters.update((list) => [...list, newCharacter]);
-    this.resetFields();
-
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  public dragonballService = inject(DragonballService);
 
 }
