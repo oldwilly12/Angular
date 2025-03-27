8cfbb9d4de042c6ff05e8ec476f34f3caad0bd64
Fracisco Acosta <rwilly203@gmail.com>
2025-03-26 18:00:07 -0600

    starting seccion 7
    starting seccion 7

--- gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SideMenuComponent,
+  ],
   templateUrl: './dashboard-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
---
+++ gifs-app/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  giphyApiKey: 'k5DaKbqe4cP38Oh5CrbRHJmmUjUfxEvp',
+  giphyUrl: 'https://api.giphy.com/v1'
+};
